736f80f0331ae0cba117718b7ea146a2829d3f17
prabhatmauryaa <prabhatmaurya165@gmail.com>
2026-03-24 22:56:48 +0530

    update hero section

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -11,12 +11,12 @@ const Hero = () => {
         {/* Background Image Container */}
         <div className="absolute inset-0 z-0">
           <img
-            src="/hero_image_4.png"
+            src="hero_section_freepik.jpg"
             alt="Vastra Culture Hero"
             className="w-full h-full object-cover object-center"
           />
           {/* Dark Overlay - Adjusted opacity to make left-aligned text pop */}
-          <div className="absolute inset-0 bg-black/40 backdrop-blur-[1px]"></div>
+          <div className="absolute inset-0 bg-black/20"></div>
         </div>
 
         {/* Content Container - Changed to items-start for left alignment */}
@@ -24,19 +24,19 @@ const Hero = () => {
 
           {/* Headline */}
           <div className="max-w-3xl">
-            <h1 className="text-2xl md:text-3xl lg:text-5xl font-black text-white mb-6 tracking-tighter uppercase">
-              Vastra <span className="text-indigo-400">Culture</span>
+            <h1 className="text-[10px] md:text-3xl font-bold uppercase tracking-[0.2em] text-black mb-2 py-6">
+              Luxury Hits Different
             </h1>
           </div>
 
           {/* Description */}
-          <p className="max-w-xl text-sm md:text-lg text-gray-200 mb-10 leading-relaxed font-medium">
+          {/* <p className="max-w-xl text-sm md:text-lg text-gray-200 mb-10 leading-relaxed font-medium">
             Experience the next generation of Gen-Z fashion. High-quality streetwear delivered with real-time updates.
-          </p>
+          </p> */}
 
           {/* The Responsive Button - Left Aligned on Desktop */}
-          <button className="group relative inline-flex item-center justify-center px-4 py-2 text-base md:text-lg font-bold text-black transition-all duration-300 bg-white hover:bg-gray-400
-          rounded-full shadow-2xl hover:scale-105 transform">
+          <button className="group relative inline-flex item-center justify-center px-4 py-2  text-base md:text-lg font-bold text-black transition-all duration-300 bg-white hover:bg-gray-400
+          rounded-xl shadow-2xl hover:scale-105 transform">
             Explore Now
           </button>
         </div>
